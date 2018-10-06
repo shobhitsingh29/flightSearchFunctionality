0157c9d95803b186d752213e5bc1824b5ec065ee
shobhitsingh <shobhit.singh@paytm.com>
2018-10-07 00:15:10 +0530

    init1ss

---
+++ flightsearch/src/@components/@datePicker/datePicker.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './datePicker.scss';
+
+class DatePicker extends Component {
+  render() {
+    return (
+        <input type="date"/>
+    );
+  }
+}
+
+export default DatePicker;
--- flightsearch/src/leftPanel/leftPanel.jsx
+++ flightsearch/src/leftPanel/leftPanel.jsx
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import InputBox from '../@components/@inputBox/InputBox';
 import Button from '../@components/@button/button';
+import DatePicker from '../@components/@datePicker/datePicker';
+import PriceRange from '../@components/@priceRange/priceRange';
+
 import './leftPanel.css';
 
 class LeftPanel extends Component {
@@ -15,9 +18,29 @@ class LeftPanel extends Component {
             <InputBox placeholder="origin"></InputBox>
             <InputBox placeholder="destination"></InputBox>
           </div>
+          <div>
+            origin Date <DatePicker></DatePicker>
+            destination Date <DatePicker></DatePicker>
+          </div>
+          <div>
+            passenger
+            <div>
+              <Button>+</Button>
+              <Button>-</Button>
+            </div>
+          </div>
+          <div>
+            price range
+            <PriceRange min={0}>
+            </PriceRange>
+          </div> <div>
+            <Button>
+              Search
+            </Button>
+          </div>
         </div>
-    );
+  );
   }
-}
+  }
 
-export default LeftPanel;
+  export default LeftPanel;
