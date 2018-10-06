181e363b88ebe8b1e3b8bbac20e40fbeb2faf5e5
Sarthak Khandelwal <sarthak.khandelwal@paytm.com>
2018-10-06 23:39:12 +0530

    change in appjs

--- flightsearch/src/rightPanel/flightItem.jsx
+++ flightsearch/src/rightPanel/flightItem.jsx
@@ -4,10 +4,12 @@ import './rightPanel.css';
 class FlightListing extends Component {
   render() {
   	const {singleFlightData} = this.props;
+    const {price, number, return_trip :{number: returnTripNumber}} = singleFlightData;
     console.log('singleFlightData', singleFlightData);
     return (
       <div className="singleFlightContainer">
-
+        <div>{price}</div>
+        
       </div>
     );
   }
