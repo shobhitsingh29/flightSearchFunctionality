190103a3fa2fc75134e4ee55bf12408222771fc4
shobhitsingh <shobhit.singh@paytm.com>
2018-10-06 23:55:25 +0530

    init1

--- flightsearch/src/leftPanel/leftPanel.jsx
+++ flightsearch/src/leftPanel/leftPanel.jsx
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import InputBox from '../@components/@inputBox/InputBox';
 import Button from '../@components/@button/button';
 import './leftPanel.css';
 
 class LeftPanel extends Component {
   render() {
     return (
-      <div >
-        <Button >Oneway</Button>
-        <Button >Return</Button>
-      </div>
+        <div className="border">
+          <div>
+            <Button>Oneway</Button>
+            <Button>Return</Button>
+          </div>
+          <div>
+            <InputBox placeholder="origin"></InputBox>
+            <InputBox placeholder="destination"></InputBox>
+          </div>
+        </div>
     );
   }
 }
